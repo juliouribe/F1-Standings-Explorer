eeb2b86df3a0e5ed6b14e572a764e123b7559b61
Julio Uribe <uribejulioc@gmail.com>
2025-07-18 10:17:09 -0700

    Add custom user model in case we ever want it

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User
+AUTH_USER_MODEL = "accounts.CustomUser"
