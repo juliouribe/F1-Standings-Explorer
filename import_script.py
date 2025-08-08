3c1f3b90f04fb84bdb470c31972f16a9f595af50
Julio Uribe <uribejulioc@gmail.com>
2025-08-07 22:38:51 -0700

    Start reworking the import script into an actual import script

--- import_script.py
+++ import_script.py
@@ -7,15 +7,59 @@ CONSTRUCTOR_ENDPOINT = "/api/constructors/create/"
 RACE_TRACK_ENDPOINT = "/api/races/race_tracks/create/"
 GRAND_PRIX_ENDPOINT = "/api/races/grand_prix/create/"
 
+ERGAST_URL = "https://api.jolpi.ca/ergast/f1/"
 
-def create_object(data):
+
+def query_third_party(year):
+    url = f"{ERGAST_URL}/{year}/results"
+    try:
+        response = requests.get(
+            url,
+        )
+        print("Making Third Party API call...")
+        print(f"Status Code: {response.status_code}")
+        print(f"Response Headers: {dict(response.headers)}")
+
+        if response.status_code == 201:
+            print("Query completed successfully")
+            data = response.json()
+            races = data.get("MRData", {}).get("RaceTable", {}).get("Races", [])
+            total_race_results = sum(len(race.get("Results", [])) for race in races)
+            print(f"Retrieve {total_race_results} race results")
+        elif response.status_code == 400:
+            print("❌ Validation errors:")
+            print(json.dumps(response.json(), indent=2))
+        else:
+            print(f"❌ Unexpected response: {response.status_code}")
+            print(response.text)
+
+    except requests.exceptions.ConnectionError:
+        print("❌ Connection error - make sure your Django server is running")
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Request error: {e}")
+    except json.JSONDecodeError:
+        print("❌ Invalid JSON response")
+        print(response.text)
+
+def parse_data(data):
+    parsed_data = []
+
+    return parsed_data
+
+
+def write_data(parsed_data):
+    url = f"{BASE_URL}{GRAND_PRIX_ENDPOINT}"
     headers = {
         "Content-Type": "application/json",
     }
-    url = f"{BASE_URL}{data["endpoint"]}"
 
     try:
-        response = requests.post(url, json=data, headers=headers)
+        response = requests.post(
+            url,
+            json=parsed_data,
+            headers=headers,
+        )
+        print("Making write requests to Django Backend...")
         print(f"Status Code: {response.status_code}")
         print(f"Response Headers: {dict(response.headers)}")
 
@@ -147,7 +191,13 @@ def main():
             },
         ],
     }
-    create_object(test_data_grand_prix_two)
+    """
+    Make API call to get the data
+    Parse data into another iterable with formatting that matches endpoint
+    Iterate over GPs and make repeated calls
+    Make sure it's synchronous to avoid potential issues in get or create calls
+    """
+    rez = query_third_party(test_data_grand_prix_two)
 
 
 if __name__ == "__main__":
