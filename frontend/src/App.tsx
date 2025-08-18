ded880213453b6a7779c57f046b58e8ff65e6f63
Julio Uribe <uribejulioc@gmail.com>
2025-08-18 08:32:33 -0700

    Add SeasonHeader component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import RaceResults from "./components/RaceResults";
+import SeasonHeader from "./components/SeasonHeader";
 
 function App() {
   return (
     <>
+      <SeasonHeader />
       <RaceResults />
     </>
   );
---
+++ frontend/src/components/SeasonHeader.tsx
@@ -0,0 +1,10 @@
+const SeasonHeader = () => {
+  return (
+    <div className="flex items-center">
+      <img className="w-3xs" src="banner-f1.png" alt="" />
+      <h1 className="text-3xl">F1 Standings Explorer</h1>
+    </div>
+  );
+};
+
+export default SeasonHeader;
