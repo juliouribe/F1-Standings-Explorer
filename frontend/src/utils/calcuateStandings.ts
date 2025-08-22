94290dd3a55f30f28292996de326205c35ae0d71
Julio Uribe <uribejulioc@gmail.com>
2025-08-22 12:23:04 -0700

    Fix bug where driver points total fall off when missing a race result in the calendar

--- frontend/src/utils/calcuateStandings.ts
+++ frontend/src/utils/calcuateStandings.ts
@@ -1,8 +1,6 @@
 import type { GrandPrix, ProcessedData } from "@/types";
 import { abbreviateGrandPrixName, isDateBetween } from "./stringUtils";
 
-// TODO: Write an interface to replace Record<string, any> with something descriptive.
-
 function calculateStandings(
   races: GrandPrix[],
   start: string,
@@ -16,7 +14,6 @@ function calculateStandings(
   const racesFiltered = races.filter((race) =>
     isDateBetween(race.date, start, end)
   );
-
   // Iterate over races and find how many races and drivers there are.
   for (const race of racesFiltered) {
     const acronym = abbreviateGrandPrixName(race.name);
@@ -36,10 +33,12 @@ function calculateStandings(
   // Build out matrices to hold driver points across races.
   // These matrices are formatted for data visualizations using Chart JS.
   const positionMatrix: Record<string, number[]> = {}; // driverID -> array of points per race
+  const driverSeenTemplate: Record<string, boolean> = {}; // driverID -> boolean
   const cumulativeMatrix: Record<string, number[]> = {}; // driverID -> array of cumalitive points
   Object.keys(driverNameMap).forEach((driverID) => {
     positionMatrix[driverID] = new Array(racesFiltered.length + 1).fill(""); // one extra for the total
     cumulativeMatrix[driverID] = new Array(racesFiltered.length).fill(0);
+    driverSeenTemplate[driverID] = false;
   });
 
   // Build out matrix for constructors cumulative points.
@@ -54,14 +53,15 @@ function calculateStandings(
   });
 
   for (const [idx, race] of racesFiltered.entries()) {
-    // if (!isDateBetween(race.date, start, end)) continue;
     const teamSeen = { ...teamSeenTemplate };
+    const driverSeen = { ...driverSeenTemplate };
     for (const result of race.race_results) {
       const driverID = result.driver.short_name;
       const constructor = result.constructor.name;
       positionMatrix[driverID][idx] = result.finish_position;
       const prevTotal = idx > 0 ? cumulativeMatrix[driverID][idx - 1] : 0;
       cumulativeMatrix[driverID][idx] = prevTotal + result.points;
+      driverSeen[driverID] = true;
 
       // First time we see a team, we grab prev total. Second go, update current
       if (teamSeen[constructor]) {
@@ -77,6 +77,11 @@ function calculateStandings(
       if (idx == racesFiltered.length - 1)
         positionMatrix[driverID][idx + 1] = cumulativeMatrix[driverID][idx];
     }
+    // When a driver doesn't have a race result, preserve their previous cumulative points total.
+    Object.entries(driverSeen).map(([driverID, seen]) => {
+      const prevTotal = idx > 0 ? cumulativeMatrix[driverID][idx - 1] : 0;
+      if (!seen) cumulativeMatrix[driverID][idx] = prevTotal;
+    });
   }
 
   // Sort drivers and teams from first to last in standings, descending order.
