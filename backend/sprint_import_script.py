05a62da911551d5c6619c3eca3d5cb9b5df11f3c
Julio Uribe <uribejulioc@gmail.com>
2025-08-31 13:06:53 -0700

    Add cache clear to import scripts

--- backend/sprint_import_script.py
+++ backend/sprint_import_script.py
@@ -10,6 +10,8 @@ import argparse
 import json
 import requests
 import time
+from django.core.cache import cache
+
 
 BASE_URL = "http://localhost:8000"
 DRIVER_ENDPOINT = "/api/drivers/create/"
@@ -205,6 +207,7 @@ def main():
     if len(data) >= 0:
         parsed_data = parse_data(data)
         write_data(parsed_data)
+        cache.clear()
 
 
 if __name__ == "__main__":
