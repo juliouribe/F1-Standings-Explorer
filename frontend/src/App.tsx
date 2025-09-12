de495583b405cd7c133f4e8ab318d98b9c69872b
Julio Uribe <uribejulioc@gmail.com>
2025-09-12 15:32:01 -0700

    Add Vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import SeasonView from "./components/SeasonView";
 import SeasonHeader from "./components/SeasonHeader";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="max-w-full">
+      <Analytics />
       <SeasonHeader />
       <SeasonView />
     </div>
